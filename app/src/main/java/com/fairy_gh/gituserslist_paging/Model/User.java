dcfed078a0336d2c62674b1faffa3a50a90d4ebb
fereshte gholami <f.gholami.dev@gmail.com>
2021-02-24 00:14:43 -0800

    add user model class

---
+++ app/src/main/java/com/fairy_gh/gituserslist_paging/Model/User.java
@@ -0,0 +1,60 @@
+package com.fairy_gh.gituserslist_paging.Model;
+
+import androidx.databinding.BindingAdapter;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import com.squareup.picasso.Picasso;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class User {
+
+    @SerializedName("id")
+    @Expose
+    public int id;
+
+    @SerializedName("login")
+    @Expose
+    public String login;
+
+    @SerializedName("avatar_url")
+    @Expose
+    public String avatar_url;
+
+    public User(int id, String login, String avatar_url) {
+        this.id = id;
+        this.login = login;
+        this.avatar_url = avatar_url;
+    }
+
+    @BindingAdapter("CircleImageView:image")
+    public static void avatarBinding(CircleImageView circleImageView, String avatar){
+        Picasso.get().load(avatar).into(circleImageView);
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getAvatar_url() {
+        return avatar_url;
+    }
+
+    public void setAvatar_url(String avatar_url) {
+        this.avatar_url = avatar_url;
+    }
+}
+
+
