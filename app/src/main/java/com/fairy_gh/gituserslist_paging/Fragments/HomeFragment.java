f75205c3f74a2a03128819a4cd81109eb2879319
fereshte gholami <f.gholami.dev@gmail.com>
2021-02-24 05:18:13 -0800

    add HomeFragment.java & fragment_home.xml

---
+++ app/src/main/java/com/fairy_gh/gituserslist_paging/Fragments/HomeFragment.java
@@ -0,0 +1,24 @@
+package com.fairy_gh.gituserslist_paging.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fairy_gh.gituserslist_paging.R;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
