ac55db56d64c4544ab51828817c7c3423e00e457
fereshte gholami <f.gholami.dev@gmail.com>
2021-02-24 21:44:37 -0800

    create methods of RetrofitClient.java

--- app/src/main/java/com/fairy_gh/gituserslist_paging/Api/RetrofitClient.java
+++ app/src/main/java/com/fairy_gh/gituserslist_paging/Api/RetrofitClient.java
@@ -1,4 +1,31 @@
 package com.fairy_gh.gituserslist_paging.Api;
 
+import com.fairy_gh.gituserslist_paging.Model.User;
+import java.util.List;
+import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory;
+import io.reactivex.Observable;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitClient {
+
+    private String base_url = "https://api.github.com/";
+    private static Retrofit instance = null;
+
+    public Retrofit getInstance(String url){
+
+        if(instance == null){
+            instance = new Retrofit.Builder()
+                    .baseUrl(url)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                    .build();
+        }
+
+        return instance;
+    }
+
+    public ApiInterface getApiInterface(){
+        return instance.create(ApiInterface.class);
+    }
 }
