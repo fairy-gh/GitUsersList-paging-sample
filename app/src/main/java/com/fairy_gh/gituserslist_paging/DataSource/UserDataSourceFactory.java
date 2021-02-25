693ffa318c8ce691cba8d31c4d985e537adfcee0
fereshte gholami <f.gholami.dev@gmail.com>
2021-02-25 08:00:38 -0800

    add codes to UserDataSourceFactory.java

--- app/src/main/java/com/fairy_gh/gituserslist_paging/DataSource/UserDataSourceFactory.java
+++ app/src/main/java/com/fairy_gh/gituserslist_paging/DataSource/UserDataSourceFactory.java
@@ -1,4 +1,23 @@
 package com.fairy_gh.gituserslist_paging.DataSource;
 
-public class UserDataSourceFactory {
+import androidx.annotation.NonNull;
+import androidx.lifecycle.MutableLiveData;
+import androidx.paging.DataSource;
+import com.fairy_gh.gituserslist_paging.Model.User;
+
+public class UserDataSourceFactory extends DataSource.Factory<Integer, User> {
+
+    MutableLiveData<UserDataSource> userDataSourceMld = new MutableLiveData<>();
+
+    public MutableLiveData<UserDataSource> getUserDataSourceMld() {
+        return userDataSourceMld;
+    }
+
+    @NonNull
+    @Override
+    public DataSource<Integer, User> create() {
+        UserDataSource userDataSource = new UserDataSource();
+        userDataSourceMld.postValue(userDataSource);
+        return userDataSource;
+    }
 }
