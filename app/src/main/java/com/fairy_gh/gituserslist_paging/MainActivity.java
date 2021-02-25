5617c5db41a7bf3d1b345ab4a24afe64dc092446
fereshte gholami <f.gholami.dev@gmail.com>
2021-02-25 08:39:34 -0800

    define navController and it's settings

--- app/src/main/java/com/fairy_gh/gituserslist_paging/MainActivity.java
+++ app/src/main/java/com/fairy_gh/gituserslist_paging/MainActivity.java
@@ -1,14 +1,27 @@
 package com.fairy_gh.gituserslist_paging;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    NavController navController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navController = Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return super.onSupportNavigateUp();
     }
 }
