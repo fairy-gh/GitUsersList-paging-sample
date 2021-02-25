a411f95e89cad56ef000abfe830760f6b3aa27bb
fereshte gholami <f.gholami.dev@gmail.com>
2021-02-25 08:12:40 -0800

    add UserViewModel.java codes

---
+++ app/src/main/java/com/fairy_gh/gituserslist_paging/UserViewModel.java
@@ -0,0 +1,30 @@
+package com.fairy_gh.gituserslist_paging;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+import androidx.paging.LivePagedListBuilder;
+import androidx.paging.PagedList;
+
+import com.fairy_gh.gituserslist_paging.DataSource.UserDataSource;
+import com.fairy_gh.gituserslist_paging.DataSource.UserDataSourceFactory;
+import com.fairy_gh.gituserslist_paging.Model.User;
+
+public class UserViewModel extends ViewModel {
+
+    public static LiveData<PagedList<User>> userPagedList;
+
+    public UserViewModel(){
+        UserDataSourceFactory dataSourceFactory = new UserDataSourceFactory();
+        PagedList.Config config = new PagedList.Config.Builder()
+                .setPageSize(UserDataSource.per_page)
+                .setEnablePlaceholders(true)
+                .build();
+
+        userPagedList = new LivePagedListBuilder(dataSourceFactory, config).build();
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+    }
+}
