f6b19ef0021bc0bb1e77fa4354a4d59bd62342a1
fereshte gholami <f.gholami.dev@gmail.com>
2021-02-25 08:32:06 -0800

    add HomeFragment.java codes

--- app/src/main/java/com/fairy_gh/gituserslist_paging/Fragments/HomeFragment.java
+++ app/src/main/java/com/fairy_gh/gituserslist_paging/Fragments/HomeFragment.java
@@ -2,13 +2,30 @@ package com.fairy_gh.gituserslist_paging.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.Observable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.paging.PagedList;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.fairy_gh.gituserslist_paging.Adapter.UserAdapter;
+import com.fairy_gh.gituserslist_paging.Model.User;
 import com.fairy_gh.gituserslist_paging.R;
+import com.fairy_gh.gituserslist_paging.UserViewModel;
+import com.fairy_gh.gituserslist_paging.databinding.FragmentHomeBinding;
+
+import static androidx.recyclerview.widget.RecyclerView.*;
 
 public class HomeFragment extends Fragment {
 
@@ -16,9 +33,31 @@ public class HomeFragment extends Fragment {
         // Required empty public constructor
     }
 
+    FragmentHomeBinding binding;
+    UserViewModel viewModel;
+    UserAdapter adapter = new UserAdapter(getActivity());;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.recyclerView.setHasFixedSize(true);
+        binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+
+        viewModel = new ViewModelProvider(getActivity()).get(UserViewModel.class);
+        viewModel.userPagedList.observe(getActivity(), new Observer<PagedList<User>>() {
+            @Override
+            public void onChanged(PagedList<User> users) {
+                adapter.submitList(users);
+            }
+        });
+
     }
 }
