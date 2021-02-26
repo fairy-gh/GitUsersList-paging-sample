9fc88e5a857b49a5a3aac1eea75fa3860306ae25
fereshte gholami <f.gholami.dev@gmail.com>
2021-02-25 21:18:56 -0800

    add @Headers to ApiInterface.java

--- app/src/main/java/com/fairy_gh/gituserslist_paging/Api/ApiInterface.java
+++ app/src/main/java/com/fairy_gh/gituserslist_paging/Api/ApiInterface.java
@@ -1,14 +1,17 @@
 package com.fairy_gh.gituserslist_paging.Api;
 
-import com.fairy_gh.gituserslist_paging.Model.User;
+import com.fairy_gh.gituserslist_paging.model.User;
+
 import java.util.List;
 
-import io.reactivex.Observable;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.Query;
 
 public interface ApiInterface {
+    @Headers({"Accept: application/vnd.github.v3+json"})
     @GET("users")
     Call<List<User>> getUsers(
             @Query("since") int userId,
